c31a1c9e0bfed1ca551161b7c8f1ec1342117678
sahil1202-12 <78477505+sahil1202-12@users.noreply.github.com>
2021-10-04 11:12:12 +0530

    Add files via upload

---
+++ sam.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int 1;i<=5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
